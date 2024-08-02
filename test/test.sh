e4a180da64a428073cb0b6b42ad1dd6b03d51984
niehwenyu <niehwenyu@163.com>
2024-08-03 01:58:49 +0800

    JunitTest脚本

--- test/test.sh
+++ test/test.sh
@@ -2,11 +2,11 @@
 
 echo "执行JUnitTest"
 
-sudo cp Auth-Service/target/surefire-reports/*.xml ../test
-sudo cp Course-Service/target/surefire-reports/*.xml ../test
-sudo cp Gitlab-Service/target/surefire-reports/*.xml ../test
-sudo cp Grade-Service/target/surefire-reports/*.xml ../test
-sudo cp Task-Service/target/surefire-reports/*.xml ../test
-sudo cp Team-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Auth-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Course-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Gitlab-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Grade-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Task-Service/target/surefire-reports/*.xml ../test
+sudo cp ../Team-Service/target/surefire-reports/*.xml ../test
 
 junit *.xml
