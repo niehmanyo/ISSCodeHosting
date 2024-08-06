294aa2a0f137c9b15ec09a5321f7b07774239d46
niehwenyu <niehwenyu@163.com>
2024-08-06 15:56:37 +0800

    test CI/CD

---
+++ Team-Service/src/test/java/com/iss/team/controller/TeamControllerTest.java
@@ -0,0 +1,44 @@
+package com.iss.team.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iss.team.service.ITeamService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TeamController.class)
+class TeamControllerTest {
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ITeamService teamService;
+
+    @InjectMocks
+    private TeamController teamController;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(teamController).build();
+    }
+
+
+}
